e8936e35234bc2e3f30b9ae7d6333c217a54e2ca
Minh <minhvu10195@gmail.com>
2017-01-21 13:25:55 +0700

    push code

---
+++ console/migrations/m170121_054022_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170121_054022_user_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170121_054022_user_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
